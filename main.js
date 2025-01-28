52af62fda2bc9fd8a8b5ac92ffc8ab0e0a8c56fe
Milagros Weinzettel <milagroswein@gmail.com>
2025-01-28 19:16:21 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,44 @@
+function comprar() {
+    let pago = true;
+    let intentos = 1;
+
+
+    let pesos = parseInt(prompt("¿Con cuánto va a abonar?"));
+
+    if (pesos = null) {
+        alert("no cuenta con saldo suficiente");
+
+
+    }
+
+
+    if (pesos => 10000) {
+        alert("puede realizar la compra");
+        pago = false;
+
+
+
+
+
+
+
+    }
+
+
+
+    else {
+        alert("no cuenta con saldo suficiente");
+
+
+
+
+    }
+
+
+}
+
+
+
+
+
+comprar()
